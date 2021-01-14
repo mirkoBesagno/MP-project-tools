e2004bc0fd82d092ca3a825b9cafd46c22c9dba9
mirko <50323497+mirkoBesagno@users.noreply.github.com>
2021-01-14 09:05:46 +0100

    .

--- Front2.0/mp-project-tools/src/app/componenti/entita/entita.component.ts
+++ Front2.0/mp-project-tools/src/app/componenti/entita/entita.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AttributoModel } from '../../model/typeorm/model/Model';
+import { ProgettoComponent } from '../progetto/progetto.component';
 
 @Component({
   selector: 'app-entita',
   templateUrl: './entita.component.html',
   styleUrls: ['./entita.component.css']
 })
-export class EntitaComponent implements OnInit {
+export class EntitaComponent implements AfterViewInit {
+
+  @ViewChild('tiopoEntita') tiopoEntita: ElementRef;
+
+  @ViewChild('tipoAttributo') tipoAttributo: ElementRef;
+  @ViewChild('tipologiaAttributo') tipologiaAttributo: ElementRef;
+  @ViewChild('nomeAttributo') nomeAttributo: ElementRef;
+
+  @ViewChild('nomeEntita') nomeEntita: ElementRef;
+
+  listaAttributi: AttributoModel[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(): void {
 
+  }
+  /* ngOnInit(): void {
+  } */
+  AggiungiEntita() {
+
+    ProgettoComponent.progetto.typeorm.AggiungiEntita_model_repository_controller(
+      ProgettoComponent.progetto.nome, this.listaAttributi
+    );
+  }
+  AggiungiAttributo(){
+    this.listaAttributi.push(new AttributoModel(this.nomeAttributo.nativeElement.value,
+      this.tipoAttributo.nativeElement.value,
+      this.tipologiaAttributo.nativeElement.value));
+
+  }
 }
